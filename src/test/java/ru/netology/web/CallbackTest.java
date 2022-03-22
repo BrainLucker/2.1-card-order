fed6459bc2ad72e1ae9b086208d909284f427231
Maksim Ivlev <brainlucker@gmail.com>
2022-03-22 23:46:14 +0300

    Validation tests added

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -2,6 +2,8 @@ package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -9,7 +11,7 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CallbackTest {
     @Test
-    void shouldSubmitRequest() {
+    void shouldSubmitValidForm() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Ивлев Мак-сим");
@@ -17,5 +19,57 @@ public class CallbackTest {
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена!"));
+    }
+
+    @Test
+    void shouldNotSubmitEmptyForm() {
+        open("http://localhost:9999");
+        $(".form").$(".button").click();
+        $("[data-test-id=order-success]").shouldNot(exist);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "On English, Ivlev Maksim",
+            "With symbol, Ивлев М@ксим",
+            "Only spaces, '  '",
+    })
+    void shouldShowErrorIfInputtedInvalidName(String testName, String name) {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue(name);
+        form.$("[data-test-id=phone] input").setValue("+79991234567");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id=name]").shouldHave(cssClass("input_invalid"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Without plus, 79991234567",
+            "With letter, +7999123456o",
+            "With symbol, +7999123456.",
+            "Too short, +7999123456",
+            "Too long, +799912345678",
+            "Only Spaces, '  '",
+    })
+    void shouldShowErrorIfInputtedInvalidPhone(String testName, String phone) {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Ивлев Максим");
+        form.$("[data-test-id=phone] input").setValue(phone);
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id=phone]").shouldHave(cssClass("input_invalid"));
+    }
+
+    @Test
+    void shouldChangeCheckboxTextColorIfNotChecked() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Ивлев Максим");
+        form.$("[data-test-id=phone] input").setValue("+79991234567");
+        form.$(".button").click();
+        form.$("[data-test-id=agreement]").shouldHave(cssClass("input_invalid"));
     }
 }
