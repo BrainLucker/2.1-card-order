fc1438c6a6f614a30f39f0c6e961698272a24183
Maksim Ivlev <brainlucker@gmail.com>
2022-03-26 10:35:06 +0300

    Added text to assertions

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -30,13 +30,13 @@ public class CallbackTest {
     void shouldNotSubmitEmptyForm() {
         $(".form").$(".button").click();
         $("[data-test-id=order-success]").shouldNot(exist);
+        $(".form").$("[data-test-id='name'].input_invalid .input__sub").shouldBe(visible).shouldHave(text("Поле обязательно для заполнения"));
     }
 
     @ParameterizedTest
     @CsvSource({
             "On English, Ivlev Maksim",
-            "With symbol, Ивлев М@ксим",
-            "Only spaces, '  '",
+            "With symbol, Ивлев М@ксим"
     })
     void shouldShowErrorIfInputtedInvalidName(String testName, String name) {
         SelenideElement form = $(".form");
@@ -44,7 +44,17 @@ public class CallbackTest {
         form.$("[data-test-id=phone] input").setValue("+79991234567");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        form.$("[data-test-id='name'].input_invalid .input__sub").shouldBe(visible);
+        form.$("[data-test-id='name'].input_invalid .input__sub").shouldBe(visible).shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldShowErrorIfInputtedEmptyName() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("  ");
+        form.$("[data-test-id=phone] input").setValue("+79991234567");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id='name'].input_invalid .input__sub").shouldBe(visible).shouldHave(text("Поле обязательно для заполнения"));
     }
 
     @ParameterizedTest
@@ -53,8 +63,7 @@ public class CallbackTest {
             "With letter, +7999123456o",
             "With symbol, +7999123456.",
             "Too short, +7999123456",
-            "Too long, +799912345678",
-            "Only Spaces, '  '",
+            "Too long, +799912345678"
     })
     void shouldShowErrorIfInputtedInvalidPhone(String testName, String phone) {
         SelenideElement form = $(".form");
@@ -62,7 +71,17 @@ public class CallbackTest {
         form.$("[data-test-id=phone] input").setValue(phone);
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        form.$("[data-test-id=phone].input_invalid .input__sub").shouldBe(visible);
+        form.$("[data-test-id=phone].input_invalid .input__sub").shouldBe(visible).shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678"));
+    }
+
+    @Test
+    void shouldShowErrorIfInputtedEmptyPhone() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Ивлев Максим");
+        form.$("[data-test-id=phone] input").setValue("  ");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id=phone].input_invalid .input__sub").shouldBe(visible).shouldHave(text("Поле обязательно для заполнения"));
     }
 
     @Test
